5ee238b8ba575a50b46fe4e3d5b7871d3a522405
Jonathan Baez <jbaez9910@gmail.com>
2021-02-05 13:07:08 -0500

    Mejorar la UI con filtros

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -2,7 +2,7 @@
   <table>
     <thead>
       <tr class="bg-gray-100 border-b-2 border-gray-400">
-        <th>Img</th>
+        <th></th>
         <th>
           <span>Ranking</span>
         </th>
@@ -15,24 +15,28 @@
     </thead>
     <tbody>
       <tr
+        v-for="a in assets"
+        :key="a.id"
         class="border-b border-gray-200 hover:bg-gray-100 hover:bg-orange-100"
-        v-for="(asset, idx) in assets"
-        :key="`asset-${idx}`"
       >
-        <img
-          :src="
-            `https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`
-          "
-          :alt="asset.name"
-          style="max-height: 65px"
-        />
         <td>
-          <b> #{{ asset.rank }} </b>
+          <img
+            class="w-6 h-6"
+            :src="
+              `https://static.coincap.io/assets/icons/${a.symbol.toLowerCase()}@2x.png`
+            "
+            :alt="a.name"
+          />
         </td>
-        <td>{{ asset.name }}</td>
-        <td>{{ currencyFormater(asset.priceUsd) }}</td>
-        <td>{{ numberFormat(asset.marketCapUsd) }}</td>
-        <td>{{ numberFormat(asset.changePercent24Hr) }}</td>
+        <td>
+          <b># {{ a.rank }}</b>
+        </td>
+        <td>{{ a.name }}</td>
+        <td>{{ a.priceUsd  | dollar}}</td>
+        <td>{{ a.marketCapUsd | dollar }}</td>
+        <td :class="a.changePercent24Hr.includes('-') ? 'text-red-600' : 'text-green-600'">
+          {{ a.changePercent24Hr | percent}}
+           </td>
         <td class="hidden sm:block"></td>
       </tr>
     </tbody>
@@ -42,25 +46,45 @@
 <script>
 export default {
   name: "PxAssetsTable",
+
   props: {
     assets: {
       type: Array,
       default: () => []
     }
-  },
-  methods: {
-    currencyFormater(val) {
-      return Intl.NumberFormat("en-US", {
-        currency: "USD",
-        style: "currency"
-      }).format(val);
-    },
-    numberFormat(val) {
-      if (val) {
-        return parseFloat(val).toLocaleString();
-      }
-      return val;
-    }
   }
 };
 </script>
+
+<style scoped>
+.up::before {
+  content: "👆";
+}
+
+.down::before {
+  content: "👇";
+}
+
+td {
+  padding: 20px 0px;
+  font-size: 0.6rem;
+  text-align: center;
+}
+
+th {
+  padding: 5px;
+  font-size: 0.6rem;
+}
+
+@media (min-width: 640px) {
+  td,
+  th {
+    padding: 20px;
+    font-size: 1rem;
+  }
+
+  th {
+    padding: 12px;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import "@/assets/css/tailwind.css";
 import "@/router";
 import router from "@/router";
+import {dollarFilter , percentFilter} from '@/filter';
 
+Vue.filter('dollar', dollarFilter)
+Vue.filter('percent', percentFilter)
 Vue.config.productionTip = false;
 
 new Vue({
